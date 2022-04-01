76f6b5a5fa161161430ec4ddd7fa17121b6a6d51
Roland Schätzle <80126696+roland-KA@users.noreply.github.com>
2022-04-01 18:44:17 +0200

    Adapt 2nd `fitresult`to `all_classes`

--- src/OneRule_MLJ.jl
+++ src/OneRule_MLJ.jl
@@ -11,8 +11,8 @@ end
 
 function MMI.fit(model::OneRuleClassifier, verbosity, X, y)
     tree = get_best_tree(X, y)
-    a_target_element = y[1]                                 # for producing predictions of the same type
-    fitresult = (tree, a_target_element)      
+    all_classes = MMI.classes(y[1])                         # used in `predict` to return a prediction with the same levels
+    fitresult = (tree, all_classes)      
     cache = nothing
     report = (                                              # We report ...
         tree = tree,                                        #   the OneTree
@@ -26,23 +26,23 @@ end
 
 function MMI.predict(model::OneRuleClassifier, fitresult, Xnew)
     yhat = OneRule.predict(fitresult[1], Xnew)
-    return(categorical(yhat, levels = levels(fitresult[2])))    # a CategoricalArray with same levels like the target vector
+    return(categorical(yhat, levels = fitresult[2]))        # a CategoricalArray with same levels like the target vector
 end
 
 MMI.fitted_params(::OneRuleClassifier, fitresult) = (tree = fitresult[1], features = fitresult[2])
 
 MMI.metadata_pkg.(OneRuleClassifier,
-        name       = "OneRule",
-        uuid       = "90484964-6d6a-4979-af09-8657dbed84ff",        # see your Project.toml
-        url        = "https://github.com/roland-KA/OneRule.jl",     # URL to your package repo
-        julia      = true,                                          # is it written entirely in Julia?
-        license    = "MIT",                                         # your package license
+      name       = "OneRule",
+      uuid       = "90484964-6d6a-4979-af09-8657dbed84ff",        # see your Project.toml
+      url        = "https://github.com/roland-KA/OneRule.jl",     # URL to your package repo
+      julia      = true,                                          # is it written entirely in Julia?
+      license    = "MIT",                                         # your package license
     )
 
 MMI.metadata_model(OneRuleClassifier,
-    input_scitype    = MMI.Table(MMI.Finite),
-    target_scitype   = AbstractVector{<: MMI.Finite},
-	load_path        = "OneRule.OneRuleClassifier"
+      input_scitype    = MMI.Table(MMI.Finite),
+      target_scitype   = AbstractVector{<: MMI.Finite},
+	    load_path        = "OneRule.OneRuleClassifier"
     )
 
 
